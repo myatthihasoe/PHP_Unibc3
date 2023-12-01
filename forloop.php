3de11191ec066c30fa2ee400ae6a6ef1f64a2adf
Xuan <myatthiha.soe.9@gmail.com>
2023-12-01 10:36:37 +0630

    Creating php in first Dec

---
+++ forloop.php
@@ -0,0 +1,21 @@
+<?php
+    
+    // for ($i = 0; $i < 10; $i++) {
+    //     echo $i . "<br>";
+    // }
+    // for ($i = 10; $i < 0; $i--) {
+    //     echo $i;
+    // }
+
+    //Perfect usage for each in PHP
+    $arr1 = [10, 30, 50, 65, 9, 21];
+    foreach ($arr1 as $k) {
+        echo $k;
+    }
+
+    //For each is not used array elements can be iterated as follows.
+    // for ($i = 0; $i < count($arr1)-1; $i++) {
+    //     echo $arr1[$i];
+    // }
+
+?>
---
+++ image.php
@@ -0,0 +1,16 @@
+<?php
+$count = 0;
+while ($count < 5): //start while loop
+?>
+<img src="image/dragon.jfif" width="500px" height="200px" alt="images">
+
+<?php 
+$count++;
+endwhile //end while loop
+?>
+
+<?php for ($i = 0; $i <5; $i++) : 
+    echo "<img src = 'dragon.jfif' width='300px' height='100px' alt = 'images'";
+    echo "<img src=\" dragon.jfif\" width= \"300px\" height=\"100px\" alt = 'images'";
+endfor;
+?>
